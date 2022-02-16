22f7fdeafc3f7a24e66a55577c780a9ddcb2aac0
Carlos Silva <75556369+FinalJudgement@users.noreply.github.com>
2022-02-16 18:18:36 -0500

    codewars polish

--- find_the_unique_number.js
+++ find_the_unique_number.js
@@ -1,14 +1,22 @@
 // https://www.codewars.com/kata/585d7d5adb20cf33cb000235/train/javascript
 
-function getKeyByValue(object, value) {
-  return Object.keys(object).find((key) => object[key] === value);
+function getKeyByValue(object, valueToFind) {
+  const keys = Object.keys(object);
+  // Object {3:4, 10:1}
+  // Keys {3, 10}
+  return keys.find((key) => object[key] === valueToFind);
 }
 
+// assert.strictEqual(findUniq([ 3, 10, 3, 3, 3 ]), 10);
+// object looks like: freqObj{3: 1} => {3:4, 10:1};
 function findUniq(arr) {
-  const count = {};
-  arr.forEach((x) => {
-    count[x] = (count[x] || 0) + 1;
+  const freqObj = {};
+  arr.forEach((element) => {
+    if (freqObj[element] === undefined) {
+      freqObj[element] = 1;
+    } else {
+      freqObj[element]++;
+    }
   });
-
-  return Number(getKeyByValue(count, 1));
+  return Number(getKeyByValue(freqObj, 1));
 }
